c49ff1e93760b751fc40534c0cccb694c9cfaf17
caglanurdemir <nurcagla.demir@gmail.com>
2023-01-24 17:07:01 +0300

    added footer section

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import './index.scss'
+import Kesfet from '../../SVGs/Kesfet'
+import Portal from '../../SVGs/Portal'
+import Katildiklarim from '../../SVGs/Katildiklarim'
+
+export default () => {
+    return (
+        <div className='footer-container'>
+            <div className='footer-svg' >
+                <Kesfet />
+                <p className='footer-text'>KEŞFET</p>
+            </div>
+            {/* <div className='footer-svg' >
+                <Portal />
+            </div> */}
+            <div className='footer-svg' >
+                <Katildiklarim />
+                <p className='footer-text'>DAHA CÜZDAN</p>
+            </div>
+        </div>
+    )
+}
--- src/Pages/main/index.jsx
+++ src/Pages/main/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header'
+import Footer from '../../Components/Footer'
 
 export default () => {
   return (
     <div className='main-page'>
       <Header />
+      <Footer />
     </div>
   )
 }
