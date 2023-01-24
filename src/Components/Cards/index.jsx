a97f110e980cf1a806df537cdee1efde99fd66d9
caglanurdemir <nurcagla.demir@gmail.com>
2023-01-25 02:48:20 +0300

    detail page added

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react'
 import './index.scss'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
+// import Carousel from 'flat-carousel';
+import { useNavigate } from 'react-router-dom'
 
 export default () => {
     const [data, setData] = useState(null)
+    const navigate=useNavigate()
 
     useEffect(() => {
         fetch("https://api.extrazone.com/promotions/list?Channel=PWA", {
@@ -23,27 +26,45 @@ export default () => {
 
 
     return (
-        <>
-            {data &&
-                <div>
-                    <Carousel
-                        showThumbs={false}
-                        arrows={false}
-                        infinite={false}
-                        removeArrowOnDeviceType={["tablet", "mobile"]}>
-                        {data.map((item) => {
-                            return (
-                                <div>
+    <>
+        {data &&
+            <div style={{height: '100%'}}>
+                <Carousel
+                    showThumbs={false}
+                    arrows={false}
+                    infinite={false}
+                    showStatus={false}
+                    className='carousel-images'
+                    removeArrowOnDeviceType={["tablet", "mobile"]}>
+                    {data.map((image, index) => {
+                        return (
+                            <>
+                                <div className='image0' key={index} onClick={()=>navigate('/campaign',{state:image})}>
                                     <div>
-                                        <img src={item.ImageUrl || null} />
+                                        <img className='image1' src={image.ImageUrl} />
+                                        <div className='image2' style={{ backgroundColor: image.PromotionCardColor }}>
+                                            <img className='image3' src={image.BrandIconUrl} />
+                                        </div>
+                                        <div className='image4'>
+                                            <p className='image5'>{image.RemainingText}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p className='image6'>{image.RemainingText}</p>
                                     </div>
                                 </div>
-                            )
-                        })}
-                    </Carousel>
-                </div>}
-        </>
+                                <div className='card-bottom'>
+                                    <p className='card-title noselect'>{image.Title}</p>
+                                    <p className='card-dahadaha noselect' style={{ color: image.PromotionCardColor }} >Daha Daha</p>
+                                </div>
+                            </>
+                        )
+                    })}
+                </Carousel>
+            </div>
+        }
+    </>
 
-    )
+)
 
 }
--- src/Pages/detail/index.jsx
+++ src/Pages/detail/index.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import './index.scss'
+import BackButton from '../../SVGs/Back_Button'
 
 export default () => {
+  const [data, setData] = useState(null)
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    fetch('https://api.extrazone.com/promotions?Id=' + location.state.Id, {
+      method: "GET",
+      headers: {
+        'X-Country-Id': 'TR',
+        'X-Language-Id': 'TR'
+      }
+    })
+      .then(response => response.json())
+      .then((json) => {
+        setData(json)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+
   return (
-    <div>
-      detail page
-    </div>
+    <>
+      {data &&
+        <div className='detail-page'>
+          <div className='detail-page-container'>
+            <div className='dpc-image'>
+              <img className='dpc-image-tag ' src={data.ImageUrl} />
+              <div className='dpc-circle' style={{ backgroundColor: data.BrandIconUrl }}>
+                <img src={data.BrandIconUrl} />
+              </div>
+              <div className='dpc-last-day'>
+                <p>{data.RemainingText}</p>
+              </div>
+            </div>
+            <p className='dpc-title' dangerouslySetInnerHTML={{ __html: data.Description }}></p>
+            <div style={{ padding: '10px', fontFamily: 'os-reg' }} dangerouslySetInnerHTML={{ __html: data.Description }} />
+            <div className='dpc-button'>
+              <p className='dpc-button-text'>Hemen Katıl</p>
+            </div>
+            <div className='back-button' onClick={() => navigate(-1)}>
+              <BackButton />
+            </div>
+          </div>
+        </div>
+      }
+    </>
+
   )
+
 }
