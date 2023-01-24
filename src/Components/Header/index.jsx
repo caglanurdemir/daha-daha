95445abf8e4f5c8413db07c0dbcd0b5cd8e0c295
caglanurdemir <nurcagla.demir@gmail.com>
2023-01-25 00:09:23 +0300

    isLogin control added

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 import Daha_Daha from '../../SVGs/Daha_Daha'
 import Profile from '../../SVGs/Profile'
+import GirisYap from '../../SVGs/GirisYap.svg'
 
 export default () => {
+    const [isLogin, setIsLogin] = useState(false)
     return (
         <div className='header'>
             <Daha_Daha />
-            <div className='profile'
-                style={{ backgroundColor: '#000' }}>
-                <Profile />
+            <div style={{ display: isLogin ? 'none' : 'flex', cursor: 'pointer' }}
+                onClick={() => setIsLogin(true)}>
+                <img src={GirisYap} />
             </div>
+            {isLogin ?
+
+                <div className='profile'
+                    style={{ backgroundColor: '#F40000', cursor: 'pointer' }}
+                    onClick={() => setIsLogin(false)}>
+                    <Profile />
+                </div>
+                :
+                <div className='profile'
+                    style={{ backgroundColor: '#000' }}>
+                    <Profile />
+                </div>
+            }
         </div>
     )
 }
