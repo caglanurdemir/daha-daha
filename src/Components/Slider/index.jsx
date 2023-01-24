a51c222654ed6b20d2c76e3528466cc38a74fbb6
caglanurdemir <nurcagla.demir@gmail.com>
2023-01-24 23:46:36 +0300

    fix the search button

--- src/Components/Slider/index.jsx
+++ src/Components/Slider/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './index.scss'
-import FirsatBul from '../../SVGs/FirsatBul'
+import FirsatBul from '../../SVGs/FirsatBul.svg'
 
 export default () => {
     const [data, setData] = useState(null)
@@ -31,9 +31,9 @@ export default () => {
                             <p className='item-name'>Firsat Bul</p>
                         </div>
                     </div>
-                    {data.map((item) => {
+                    {data.map((item, index) => {
                         return (
-                            <div className='item-container'>
+                            <div className='item-container' key={index}>
                                 <div className='item'>
                                     <img className='item-img' src={item.IconUrl} />
                                     <p className='item-name'>{item.Name} </p>
