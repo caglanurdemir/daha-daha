e14d7fff5995b9cca582187d78b65925ecc61b89
caglanurdemir <nurcagla.demir@gmail.com>
2023-01-24 18:33:28 +0300

    added slider to the main page

---
+++ src/Components/Slider/index.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import './index.scss'
+import FirsatBul from '../../SVGs/FirsatBul'
+
+export default () => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch("https://api.extrazone.com/tags/list", {
+            method: "GET",
+            headers: {
+                'X-Country-Id': 'TR',
+                'X-Language-Id': 'TR'
+            }
+        })
+            .then(response => response.json())
+            .then((json) => {
+                setData(json)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
+    return (
+        <>
+            {data &&
+                <div className='slider'>
+                    <div className='item-container'>
+                        <div className='item'>
+                            <img className='item-img' src={FirsatBul} />
+                            <p className='item-name'>Firsat Bul</p>
+                        </div>
+                    </div>
+                    {data.map((item) => {
+                        return (
+                            <div className='item-container'>
+                                <div className='item'>
+                                    <img className='item-img' src={item.IconUrl} />
+                                    <p className='item-name'>{item.Name} </p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+        </>
+
+    )
+
+}
--- src/Pages/main/index.jsx
+++ src/Pages/main/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../Components/Header'
+import Slider from '../../Components/Slider'
+import Cards from '../../Components/Cards'
 import Footer from '../../Components/Footer'
 
 export default () => {
   return (
     <div className='main-page'>
       <Header />
+      <Slider />
+      <Cards />
       <Footer />
     </div>
   )
